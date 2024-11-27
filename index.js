cf15983ce79040c7f59bf82c8ce80301b8a0609b
Osama Ayub <usama.ayub1@ucp.edu.pk>
2024-11-27 19:02:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ button.addEventListener("click",()=>{
   fetch("https://apis.scrimba.com/bored/api/activity")
     .then(response => response.json())
     .then(data => {
-        activityTitle.classList.add("activity-title");
+      activityTitle.classList.add("activity-title");
        activityTitle.style.fontSize="17px"
        activityTitle.style.color="#FFF"
       activityTitle.textContent = data.activity
